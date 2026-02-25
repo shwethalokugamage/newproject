62ac1fa1a94fa8fca99ada9ae138c733508b9752
shwethalokugamage <shwethalokugamage@gmail.com>
2026-02-25 22:16:46 +0530

    Refactor CustomerController to use dependency injection for CustomerService

--- src/main/java/edu_icet/controller/CustomerController.java
+++ src/main/java/edu_icet/controller/CustomerController.java
@@ -2,13 +2,15 @@ package edu_icet.controller;
 
 import edu_icet.service.CustomerService;
 import edu_icet.service.CustomerServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
 
-    CustomerService service = new CustomerServiceImpl();
+    @Autowired
+    CustomerService service;
 
     @GetMapping("/customer")
     private String getName() {
--- src/main/java/edu_icet/repository/CustomerRepository.java
+++ src/main/java/edu_icet/repository/CustomerRepository.java
@@ -9,8 +9,6 @@ import java.util.List;
 
 public class CustomerRepository {
 
-
-
         public String getName() {
             return "Nimal";
         }
